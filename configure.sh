c0678d172c4d02737acf3409df6dc0573e661eaf
Joshua Packer <jpacker@redhat.com>
2020-10-17 02:15:42 -0400

    Bring over final changes.

--- configure.sh
+++ configure.sh
@@ -1,12 +1,21 @@
 # Author: jnpacker
 
-gitInfo=(`cat .git/FETCH_HEAD | sed 's/  */\n/g' | sed 's/ /\n/g'`)
-branch=${gitInfo[2]//\'/}
-echo "Applying branch ${gitInfo[2]//\'/} to subscribe/Running.yaml and subscribe/Hibernating.yaml"
-sed -i "s/BRANCH_NAME/${branch}/" subscribe/Running.yaml
-sed -i "s/BRANCH_NAME/${branch}/" subscribe/Hibernating.yaml
+git branch > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "Make sure you run ./configure.sh from a working Git repository root"
+  exit 1
+fi
+branch=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/\* //'`
+echo "Applying branch ${branch} to subscribe/Running.yaml and subscribe/Hibernating.yaml"
+sed -i "s/git-branch: .*$/git-branch: ${branch}/" subscribe/Running.yaml
+sed -i "s/git-branch: .*$/git-branch: ${branch}/" subscribe/Hibernating.yaml
 
-channel=${gitInfo[4]//github.com:/}
-channel=${channel/\/cluster-hibernate/}
+# Order is important. Remove the colon before we add  a new one with https
+channel=`git config --get remote.origin.url`
+if [[ "$channel" != *"https"* ]]; then
+  channel=${channel//:/\/}
+  channel=${channel/git\@/https://}
+fi
 echo "Applying channel $channel to subscribe/Channel.yaml"
-sed -i "s/GIT_USERNAME/${channel}/" subscribe/Channel.yaml
+channel=${channel//\//\\\/}
+sed -i "s/pathname: .*$/pathname: ${channel}/" subscribe/Channel.yaml
